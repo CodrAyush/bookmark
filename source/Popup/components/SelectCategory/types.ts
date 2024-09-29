b762e4afba63dfe9c7bc47c84d29d45c95de909a
Ayush Kushwaha <119109504+CodrAyush@users.noreply.github.com>
2024-09-29 12:59:34 +0530

    Delete source directory

--- source/Popup/components/SelectCategory/types.ts
+++
@@ -1,3 +0,0 @@
-export interface ISelectCategoryProps {
-  categoryList: string[];
-}
--- source/utils/makeUrl.ts
+++
@@ -1,6 +0,0 @@
-export const makeUrl = (url: string): string => {
-  if (url.startsWith('http://') || url.startsWith('https://')) {
-    return url;
-  }
-  return `https://${url}`;
-};
